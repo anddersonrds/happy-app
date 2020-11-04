7cc283cfe35184af86e40349e72819b4f294f333
Anderson Rodrigues <anddersonrds@gmail.com>
2020-11-04 00:49:54 -0300

    wip

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from '@assets/map-marker.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,13 +19,26 @@ const App: React.FC = () => (
   <View style={styles.container}>
     <MapView
       style={styles.map}
+      provider={PROVIDER_GOOGLE}
       initialRegion={{
         latitude: -3.7733133,
         longitude: -38.5585407,
         latitudeDelta: 0.008,
         longitudeDelta: 0.008,
       }}
-    />
+    >
+      <Marker
+        icon={mapMarker}
+        coordinate={{
+          latitude: -3.7733133,
+          longitude: -38.5585407,
+        }}
+      >
+        <Callout>
+          <Text>Lar das meninas</Text>
+        </Callout>
+      </Marker>
+    </MapView>
   </View>
 );
 
